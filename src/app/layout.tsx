1b47cf4e9e50e1eb8a70fcb4e262e7dfbee089a8
yulangzhu <yulangzhu@gmail.com>
2025-01-03 13:39:05 +0100

    migration created

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import db from "../lib/supabase/db";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,6 +23,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  console.log(db);
   return (
     <html lang="en">
       <body
