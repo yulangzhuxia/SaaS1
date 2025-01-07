64ba2ea2e0e04864a4adb5b9ef6572c302eb7b17
yulangzhu <yulangzhu@gmail.com>
2025-01-07 08:04:09 +0100

    landing_page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import db from "../lib/supabase/db";
+import { ThemeProvider } from "@/lib/providers/next-theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,10 +27,8 @@ export default function RootLayout({
   // console.log(db);
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          <ThemeProvider attribute='class' defaultTheme="dark" enableSystem>{children}</ThemeProvider>
       </body>
     </html>
   );
