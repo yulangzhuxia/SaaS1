1897d0c78114719a5ff8dea8cb160dca5f64b48b
yulangzhu <yulangzhu@gmail.com>
2025-01-09 07:13:18 +0100

    logos animation

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import TitleSection from '@/components/landing-page/title-section';
-import { Button } from '@/components/ui/button';
-import Image from 'next/image';
-import Banner from '../../../public/appBanner.png';
+import React from "react";
+import TitleSection from "@/components/landing-page/title-section";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Banner from "../../../public/appBanner.png";
+import { CLIENTS } from "@/lib/constants";
 
 const HomePage = () => {
-    return (
-        <section>
-            <div className="overflow-hidden
+  return (
+    <>
+      <section
+        className="overflow-hidden
         px-4
         sm:px-6
         mt-10
@@ -17,11 +19,13 @@ const HomePage = () => {
         md:justify-center
         md:items-center
         "
-        >
-            <TitleSection pill="✨Your workspace, Perfected"
-            title="All-In-One Collaboration and Productivity Platform"
-            />
-            <div className="bg-white
+      >
+        <TitleSection
+          pill="✨Your workspace, Perfected"
+          title="All-In-One Collaboration and Productivity Platform"
+        />
+        <div
+          className="bg-white
             p-[2px]
             mt-[6]
             rounded-xl
@@ -30,20 +34,21 @@ const HomePage = () => {
             to-brand-primaryBlue
             sm:w-[300px]
             "
-            >
-                <Button
-                    variant="secondary"
-                    className="w-full
+        >
+          <Button
+            variant="secondary"
+            className="w-full
                     rounded-[10px]
                     p-6
                     text-2xl
                     bg-background
                     "
-                    >
-                        Get Cypress Free
-                </Button>
-            </div>
-            <div className="md:mt-[-90px]
+          >
+            Get Cypress Free
+          </Button>
+        </div>
+        <div
+          className="md:mt-[-90px]
             sm:w-full
             w-[750px]
             flex
@@ -54,11 +59,10 @@ const HomePage = () => {
             sm:ml-0
             ml-[-50px]
             "
-            >
-                <Image src={Banner}
-                alt="Application Banner"
-                />
-                <div className="bottom-0
+        >
+          <Image src={Banner} alt="Application Banner" />
+          <div
+            className="bottom-0
                 top-[50%]
                 bg-gradient-to-t
                 dark:from-background
@@ -67,18 +71,73 @@ const HomePage = () => {
                 absolute
                 z-10
                 "
-                >
-                </div>
-            </div>
-            </div>
+          ></div>
+        </div>
+      </section>
 
-            <div className="
-            "
+      <section className="relative">
+        <div
+          className="overflow-hidden
+                flex
+                absolute
+                after:content['']
+                after:dark:from-brand-dark
+                after:to-transparent
+                after:from-background
+                after:bg-gradient-to-l
+                after:right-0
+                after:bottom-0
+                after:top-0
+                after:w-20
+                after:z-10
+                after:absolute
+
+                before:content['']
+                before:dark:from-brand-dark
+                before:to-transparent
+                before:from-background
+                before:bg-gradient-to-r
+                before:left-0
+                before:top-0
+                before:bottom-0
+                before:w-20
+                before:z-10
+                before:absolute
+                "
+        >
+          {[...Array(2)].map((arr) => (
+            <div
+              key={arr}
+              className="flex
+                                flex-nowrap
+                                animate-slide
+                        "
             >
+              {CLIENTS.map((client) => (
+                <div
+                  key={client.alt}
+                  className=" relative
+                                    w-[200px]
+                                    m-20
+                                    shrink-0
+                                    flex
+                                    items-center
+                                "
+                >
+                  <Image
+                    src={client.logo}
+                    alt={client.alt}
+                    width={200}
+                    className="object-contain max-w-none"
+                  />
+                </div>
+              ))}
             </div>
-
-        </section>
-    );
+          ))}
+        </div>
+      </section>
+    </>
+  );
 };
 
-export default HomePage
+export default HomePage;
