40f8ddaef4dce34d216c4c16bbc522e20c9151e2
yulangzhu <yulangzhu@gmail.com>
2025-01-10 22:14:54 +0100

    auth

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import React from "react";
+
+const LoginPage = () => {
+  const router = useRouter();
+  return <div>LoginPage</div>;
+};
+
+export default page;
